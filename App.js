45d61aacb16ba77524012aa135fa600eab4e100e
˜Ricardo <ricardocastro.ti@gmail.com>
2023-01-09 13:10:37 -0300

    Animaçao lottie

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "react-native-vector-icons";
+import LottieView from "lottie-react-native";
+import { useRef } from "react";
 
 export default function App() {
+  const lottieRef = useRef(null);
+
+  const startAnimation = () => {
+    lottieRef.current.play();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={startAnimation}>
+        <Feather name="play" size={60} color="black" />
+      </TouchableOpacity>
+      <LottieView
+        source={require("./assets/check.json")}
+        style={{ width: 200, height: 200 }}
+        loop={false}
+        ref={lottieRef}
+      />
+      <Button title="Start animation" onPress={startAnimation} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
